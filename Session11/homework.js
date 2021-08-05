9671d637c456d4dc4a69171db0d41e47fc00623b
Phuoc149 <phuocthien956@gmail.com>
2021-08-05 20:40:43 +0700

    homework s11

---
+++ Session11/homework.js
@@ -0,0 +1,14 @@
+const b1 = document.getElementById("btn1");
+const box1 = document.getElementById("box1");
+let count = 1;
+b1.onclick = function (e) {
+  box1.classList.toggle("display");
+};
+
+const b2 = document.getElementById("btn2");
+const box2 = document.getElementById("box2");
+b2.onclick = function (e) {
+  let colors = Math.floor(Math.random() * 16777215).toString(16);
+  box2.style.backgroundColor = "#" + colors;
+  console.log(colors);
+};
